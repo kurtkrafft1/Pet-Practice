16a1647c34b1c8e6b41847e5c326caf7ac83a0bd
Kurt Krafft <kkrafft6609@gmail.com>
2020-01-17 11:28:46 -0600

    pet-2

--- main.js
+++ main.js
@@ -4,5 +4,17 @@ const pet = {
     'breed': 'Labordoodle',
     'nicknames': ['doggo', 'puppers', 'Sunster', 'dog'],
     'age': 7,
-    
+    bite: function(){
+        alert(`OH SNAP! looks like ${this.name} just bit you!`)
+    },
+    bark: function(){
+        console.log(`WOOF WOOF says ${this.name}`)
+    },
+    play: function(){
+        console.log(`Looks like ${this.name} is trying to play with you!`)
+    }
 }
+
+pet.bite()
+pet.bark()
+pet.play()
